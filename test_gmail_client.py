b4c660bdcfcd99214986a2270a08ac7b53e35fc5
rachitshah07 <rachitshah1525@gmail.com>
2025-03-26 01:32:21 +0530

    removed hardcoded sender_email

--- test_gmail_client.py
+++ test_gmail_client.py
@@ -1,6 +1,9 @@
 import unittest
 from gmail_api import GmailClient
-
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL", "")
 class TestGmailClient(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -15,7 +18,7 @@ class TestGmailClient(unittest.TestCase):
         Test if we can successfully fetch a verification code from the latest email
         sent by 'SecurityServices_NoReply@adp.com'.
         """
-        code = self.client.get_verification_code_and_receiver_email(sender_email='SecurityServices_NoReply@adp.com')
+        code = self.client.get_verification_code_and_receiver_email(sender_email=SENDER_EMAIL)
         self.assertIsNotNone(code, "Verification code should not be None if the email exists.")
         print(f"\nFetched verification code: {code}")
 
